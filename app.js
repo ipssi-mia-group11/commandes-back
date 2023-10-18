ae3a66008ba07780af62e2db9a2175e4e760d7b5
vrakotoarisoa <carellien.rakotoarisoa@gmail.com>
2023-10-18 16:14:36 +0200

    init router avec connexion à mysql

--- app.js
+++ app.js
@@ -1,13 +1,38 @@
 const express = require('express');
+const mysql = require('mysql2');
 const app = express();
-const port = 3000;
 
-// Route GET pour obtenir un message
-app.get('/', (req, res) => {
-    res.json({ message: 'Bienvenue sur mon API !' });
+// Configuration de la connexion à la base de données MySQL
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'datacommande',
 });
 
-// Démarrer le serveur
+// Connexion à la base de données MySQL
+db.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('Connecté à la base de données MySQL');
+});
+
+// Route pour récupérer des données depuis la base de données
+app.get('/api/data', (req, res) => {
+    const sql = 'SELECT * FROM utilisateurs';
+    db.query(sql, (err, result) => {
+        if (err) {
+            throw err;
+        }
+        res.json(result);
+    });
+});
+
+
+// Port sur lequel l'API écoute
+const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
-  console.log(`Serveur écoutant sur le port ${port}`);
+    console.log(`Serveur démarré sur le port ${port}`);
 });
