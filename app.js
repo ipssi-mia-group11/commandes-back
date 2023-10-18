9f76c785744db81475c95290041e7614bf69bb91
Thomas Léveillé <leveille.thomas77@gmail.com>
2023-10-18 17:14:54 +0200

    fix(conf): using DB_DATABASE env instead of DATABASE

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const db = mysql.createConnection({
     port: process.env.DB_PORT,
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DATABASE,
+    database: process.env.DB_DATABASE,
 });
 
 // Connexion à la base de données MySQL
