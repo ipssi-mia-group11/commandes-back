f586c9de84af3c4c923356155548f4f4c290df0a
Thomas Léveillé <leveille.thomas77@gmail.com>
2023-10-19 09:26:30 +0200

    feat: products CRUD

--- routers/products.js
+++ routers/products.js
@@ -7,6 +7,11 @@ const router = Router();
 // router.post
 // ...
 
+const formatProduct = (product) => ({
+    ...product,
+    Prix: parseFloat(product.Prix),
+});
+
 router.get('', (req, res) => {
     const sql = 'SELECT * FROM `produits`';
     db.query(sql, (err, result) => {
@@ -14,7 +19,63 @@ router.get('', (req, res) => {
             throw err;
         }
         res.json({
-            items: result,
+            items: result.map(formatProduct),
+        });
+    });
+});
+
+router.get('/:id', (req, res) => {
+    const sql = 'SELECT * FROM `produits` WHERE `id` = ?';
+    db.query(sql, [req.params.id,], (err, result) => {
+        if (err) {
+            throw err;
+        }
+        res.json({
+            item: formatProduct(result[0]),
+        });
+    });
+});
+
+router.post('', (req, res) => {
+    const sql = 'INSERT INTO `produits` (`NomProduit`, `Prix`, `Stock`) VALUES (?, ?, ?)';
+    db.query(sql, [req.body.NomProduit, req.body.Prix, req.body.Stock], (err, result) => {
+        if (err) {
+            throw err;
+        }
+        res.json({
+            item: {
+                ID: result.insertId,
+                ...req.body,
+            },
+        });
+    });
+});
+
+router.patch('/:id', (req, res) => {
+    const sql = 'UPDATE `produits` SET `NomProduit` = ?, `Prix` = ?, `Stock` = ? WHERE `id` = ?';
+    db.query(sql, [req.body.NomProduit, req.body.Prix, req.body.Stock, req.params.id,], (err) => {
+        if (err) {
+            throw err;
+        }
+        res.json({
+            item: {
+                ...req.body,
+                ID: req.params.id,
+            },
+        });
+    });
+});
+
+router.delete('/:id', (req, res) => {
+    const sql = 'DELETE FROM `produits` WHERE `id` = ?';
+    db.query(sql, [req.params.id,], (err) => {
+        if (err) {
+            throw err;
+        }
+        res.json({
+            item: {
+                ID: req.params.id,
+            },
         });
     });
 });
