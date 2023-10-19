a697ee24c26e7d111eb6bcabe3d3879f2cb6bbe2
Thomas Léveillé <leveille.thomas77@gmail.com>
2023-10-19 09:54:52 +0200

    chore(conf): installed cors

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const cors = require('cors');
 const express = require('express');
 const env = require('./env');
 const db = require('./db');
@@ -5,6 +6,7 @@ const productsRouter = require('./routers/products');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/api/products', productsRouter);
 
