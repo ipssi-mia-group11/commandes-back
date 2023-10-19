e0fc9f1a12648f3fb40ffe4df4b0cbbe5d2ca8fc
Thomas Léveillé <leveille.thomas77@gmail.com>
2023-10-19 09:57:26 +0200

    chore(conf): added and modified middleware

--- app.js
+++ app.js
@@ -6,8 +6,13 @@ const productsRouter = require('./routers/products');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: env.CLIENT_URL,
+    optionsSuccessStatus: 200,
+}));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true, }));
+
 app.use('/api/products', productsRouter);
 
 // Route pour récupérer des données depuis la base de données
